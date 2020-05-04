3849d43c13a615e6a9ef62ede904c0089f383614
tenclar <tenclar.silva@ac.gov.br>
2020-05-04 17:23:16 -0500

    created_at e updated_at

--- api/src/models/Appointment.ts
+++ api/src/models/Appointment.ts
@@ -1,5 +1,11 @@
 // import { uuid } from 'uuidv4';
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('appointments')
 class Appointment {
@@ -12,6 +18,11 @@ class Appointment {
   @Column('timestamp with time zone')
   date: Date;
 
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
   /* constructor({ provider, date }: Omit<Appointment, 'id'>) {
     this.id = uuid();
     this.provider = provider;
