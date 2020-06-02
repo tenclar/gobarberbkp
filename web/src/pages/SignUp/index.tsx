cdb8a6fa19ffe940959896ddfcc131a42ae717d4
Tenclar <tenclarvalus@gmail.com>
2020-06-01 22:24:23 -0500

    pages

---
+++ web/src/pages/SignUp/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiLock, FiMail } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignIn: React.FC = () => {
+  function handleSubmit(data: Record<string, unknown>): void {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <Background />
+      <Content>
+        <img src={logoImg} alt="goBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu Cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="senha"
+            type="password"
+          />
+          <Button type="submit"> Cadastrar</Button>
+        </Form>
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+};
+
+export default SignIn;
--- web/src/pages/Signin/index.tsx
+++ web/src/pages/Signin/index.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import React, { useCallback } from 'react';
+import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
+import * as Yup from 'yup';
+import { Form } from '@unform/web';
 import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="goBarber" />
-      <form>
-        <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input placeholder="senha" type="password" />
-        <button type="submit"> Entrar</button>
-        <a href="forgot">Esquci minha senha</a>
-      </form>
-      <a href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
-    <Background />
-  </Container>
-);
+const SignIn: React.FC = () => {
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatóirio'),
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail Válido'),
+        password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+      });
+      await schema.validate(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="goBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            placeholder="senha"
+            type="password"
+          />
+          <Button type="submit"> Entrar</Button>
+          <a href="forgot">Esquci minha senha</a>
+        </Form>
+        <a href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
+};
+
 export default SignIn;
