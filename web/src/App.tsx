f10b6df84d2063b8d7392a5d3c4cb9ee11616cf4
Tenclar <tenclarvalus@gmail.com>
2020-06-01 22:26:08 -0500

    library

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyled from './styles/global';
 import SignIn from './pages/Signin';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => (
   <>
     <GlobalStyled />
-    <SignIn />
+
+    <SignUp />
   </>
 );
 
